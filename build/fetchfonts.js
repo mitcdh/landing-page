ac6efe539feebb45593a3a8eb092a0cdb629e2de
Mitchell Hewes <me@mitcdh.com>
2023-03-18 11:02:50 +0100

    Refactor to use subsets of google webfonts

---
+++ build/fetchfonts.js
@@ -0,0 +1,53 @@
+const https = require('https');
+const fs = require('fs');
+const querystring = require('querystring');
+const fontWeight = ':700';
+
+function uniqueChars(str) {
+  return String.prototype.concat.call(...new Set(str));
+}
+
+fs.readFile('js/greetings.json', (err, data) => {
+  if (err) throw err;
+  const json = JSON.parse(data);
+  const fontmap = {};
+  json.greetings.forEach((greeting) => {
+    const font = greeting.font.replace(/ /g, '+');
+    const message = greeting.message.replace(/\s+/g, '');
+    if (!fontmap[font]) {
+      fontmap[font] = message;
+    } else {
+      fontmap[font] += uniqueChars(message);
+    }
+  });
+
+  const fontUrls = [];
+  for (const font in fontmap) {
+    const url = `https://fonts.googleapis.com/css?family=${font}${fontWeight}&text=${querystring.escape(uniqueChars(fontmap[font]))}`;
+    fontUrls.push(url);
+  }
+
+  console.log(fontUrls);
+
+  let concatenatedFonts = '';
+  let completedRequests = 0; // added counter variable
+  fontUrls.forEach((url) => {
+    console.log("fetchfonts.js :: Fetching " + url + "...");
+    https.get(url, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        concatenatedFonts += data + '\n';
+        completedRequests++; // increment the counter
+        if (completedRequests === fontUrls.length) { // check if all requests have completed
+          fs.writeFileSync('css/webfonts.css', concatenatedFonts, { flag: 'w' });
+        }
+      });
+    }).on('error', (err) => {
+      console.log('fetchfonts.js :: Error: ' + err.message);
+    });
+  });
+
+});
--- js/greeting.js
+++ js/greeting.js
@@ -1,125 +1,144 @@
-$(function() {
-// getBrowserLocales adapted from: https://phrase.com/blog/posts/detecting-a-users-locale/#Client-side_The_navigatorlanguages_Object
-// Typing adapted from: https://usefulangle.com/post/75/typing-effect-animation-javascript-css
+$(function () {
+  // getBrowserLocales adapted from: https://phrase.com/blog/posts/detecting-a-users-locale/#Client-side_The_navigatorlanguages_Object
+  // Typing adapted from: https://usefulangle.com/post/75/typing-effect-animation-javascript-css
 
-function getBrowserLocales(options = {}) {
-  const defaultOptions = {
-    languageCodeOnly: false,
-  };
+  function getBrowserLocales(options = {}) {
+    const defaultOptions = {
+      languageCodeOnly: false,
+    };
 
-  const opt = {
-    ...defaultOptions,
-    ...options,
-  };
+    const opt = {
+      ...defaultOptions,
+      ...options,
+    };
 
-  const browserLocales =
-    navigator.languages === undefined
-      ? [navigator.language]
-      : navigator.languages;
+    const browserLocales =
+      navigator.languages === undefined
+        ? [navigator.language]
+        : navigator.languages;
 
-  if (!browserLocales) {
-    return undefined;
+    if (!browserLocales) {
+      return undefined;
+    }
+
+    return browserLocales.map((locale) => {
+      const trimmedLocale = locale.trim();
+
+      return opt.languageCodeOnly
+        ? trimmedLocale.split(/-|_/)[0]
+        : trimmedLocale;
+    });
   }
 
-  return browserLocales.map(locale => {
-    const trimmedLocale = locale.trim();
+  function shuffleArray(a) {
+    let j, t;
+    for (let i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      t = a[i];
+      a[i] = a[j];
+      a[j] = t;
+    }
+  }
 
-    return opt.languageCodeOnly
-      ? trimmedLocale.split(/-|_/)[0]
-      : trimmedLocale;
-  });
-}
-	
-function shuffleArray(a) {
-	let j, t;
-	for(let i = a.length - 1; i > 0; i--) {
-  		j = Math.floor(Math.random() * (i + 1));
-  		t = a[i];
-  		a[i] = a[j];
-  		a[j] = t;
-	}
-}
+  function Type() {
+    var text = greetingContents[currentGreeting].message.substring(
+      0,
+      greetingIndex + 1
+    );
+    textElement.style.fontFamily =
+      "'" + greetingContents[currentGreeting].font + "', sans-serif";
+    textElement.innerHTML = text;
+    greetingIndex++;
+    if (text === greetingContents[currentGreeting].message) {
+      cursorElement.style.display = "none";
+      if (greetingContents.length > 1) {
+        clearInterval(currentInterval);
+        setTimeout(function () {
+          currentInterval = setInterval(Delete, 50);
+        }, 3000);
+      }
+    }
+  }
 
-function Type() { 
-	var text =  greetingContents[currentGreeting][0].substring(0, greetingIndex + 1);
-	textElement.style.fontFamily = greetingContents[currentGreeting][2];
-	textElement.innerHTML = text;
-	greetingIndex++;
-	if(text === greetingContents[currentGreeting][0]) {
-		cursorElement.style.display = 'none';
-		if(greetingContents.length > 1)
-		{
-			clearInterval(currentInterval);
-			setTimeout(function() {
-				currentInterval = setInterval(Delete, 50);
-			}, 3000);
-		}
-	}
-}
+  function Delete() {
+    var text = greetingContents[currentGreeting].message.substring(
+      0,
+      greetingIndex - 1
+    );
+    textElement.innerHTML = text;
+    greetingIndex--;
+    if (text === "") {
+      clearInterval(currentInterval);
+      if (currentGreeting == greetingContents.length - 1) {
+        currentGreeting = 0;
+      } else {
+        currentGreeting++;
+      }
+      greetingIndex = 0;
+      setTimeout(function () {
+        cursorElement.style.display = "inline-block";
+        currentInterval = setInterval(Type, 100);
+      }, 200);
+    }
+  }
 
-function Delete() {
-	var text =  greetingContents[currentGreeting][0].substring(0, greetingIndex - 1);
-	textElement.innerHTML = text;
-	greetingIndex--;
-	if(text === '') {
-		clearInterval(currentInterval);
-		if(currentGreeting == (greetingContents.length - 1))
-			currentGreeting = 0;
-		else
-			currentGreeting++;
-		
-		greetingIndex = 0;
-		setTimeout(function() {
-			cursorElement.style.display = 'inline-block';
-			currentInterval = setInterval(Type, 100);
-		}, 200);
-	}
-}
-	
-var currentGreeting = 0;
-var currentInterval;
-var greetingIndex = 0;
+  async function getGreetings() {
+    try {
+      const response = await fetch("js/greetings.json");
+      const data = await response.json();
+      return data.greetings.map((greeting) => ({
+        message: greeting.message,
+        code: greeting.code,
+        font: greeting.font,
+      }));
+    } catch (error) {
+      console.error(error);
+      return [
+        {
+          message: "Hello World!",
+          code: "en",
+          font: "sans-serif",
+        },
+      ];
+    }
+  }
 
-var textElement = document.querySelector(".greeting__text");
-var cursorElement = document.querySelector(".greeting__cursor");
-var browserLanguages = getBrowserLocales({languageCodeOnly: true});
-var uniqueLanguages = (typeof browserLanguages !== "undefined") ? Array.from(new Set(browserLanguages)) : ["en"];
-	
-textElement.innerHTML = "";
+  async function asyncRunner() {
+    // Greetings and language codes
+    greetingContents = await getGreetings();
 
-// Greetings and language codes
-var greetingContents = [ 
-	["G'day Mates", "en", "'Noto Sans', sans-serif"],
-	["Ciao Amici", "it", "'Noto Sans', sans-serif"],
-	["Salut les Amis", "fr", "'Noto Sans', sans-serif"],
-	["¡Hola Amigos!", "es", "'Noto Sans', sans-serif"],
-	["Привет друзья", "ru", "'Noto Sans', sans-serif"],
-	["大家好", "zh", "'Noto Sans TC', sans-serif"],
-	["مرحبا اصدقء", "ar", "'Noto Sans Arabic', sans-serif"],
-	["Servus, Liebe Freunde", "de", "'Noto Sans', sans-serif"],
-	["Alô Galera", "pt", "'Noto Sans', sans-serif"],
-	["안녕하세요 친구", "ko", "'Noto Sans KR', sans-serif"],
-	["皆さん、こんにちは", "ja", "'Noto Sans JP', sans-serif"],
-	["Hej Mina Vänner", "sv", "'Noto Sans', sans-serif"],
-	["Selam Dostlar", "tr", "'Noto Sans', sans-serif"],
-	["Pozdravljeni Prijatelji", "sl", "'Noto Sans', sans-serif"],
-];
+    // Before we start shuffle all greetings to show no preferences
+    shuffleArray(greetingContents);
 
-// Before we start shuffle all greetings to show no preferences 
-shuffleArray(greetingContents);
+    // Move greetings that match the user's languages to the front of the array
+    uniqueLanguages.reverse().forEach((language) =>
+      greetingContents.forEach(function (greeting, i) {
+        if (greeting.code === language) {
+          greetingContents.splice(i, 1);
+          greetingContents.unshift(greeting);
+        }
+      })
+    );
 
-// Now go through the users languages in reverse order and move matches to the front
-uniqueLanguages.reverse().forEach(language =>
-	greetingContents.forEach(function(greeting,i)
-	{
-		if(greeting[1] === language)
-		{
-			greetingContents.splice(i, 1);
-			greetingContents.unshift(greeting);
-		}
-	})
-);
+    // Start the type/delete cycle
+    currentInterval = setInterval(Type, 100);
+  }
 
-// Start the type/delete cycle
-currentInterval = setInterval(Type, 100);
+  var currentGreeting = 0;
+  var currentInterval;
+  var greetingIndex = 0;
+
+  var textElement = document.querySelector(".greeting__text");
+  var cursorElement = document.querySelector(".greeting__cursor");
+  var browserLanguages = getBrowserLocales({ languageCodeOnly: true });
+  var uniqueLanguages =
+    typeof browserLanguages !== "undefined"
+      ? Array.from(new Set(browserLanguages))
+      : ["en"];
+
+  var greetingContents = [];
+
+  textElement.innerHTML = "";
+
+  asyncRunner();
 });
