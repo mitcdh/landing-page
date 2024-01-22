61223bc17de641782090c269a909dce3227fb2ed
Mitchell Hewes <me@mitcdh.com>
2024-01-22 23:44:58 +0100

    Improved/more readable and commented js/css

--- js/background.js
+++ js/background.js
@@ -1,16 +1,28 @@
+// Listen for the DOMContentLoaded event to ensure the DOM is fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('data/backgrounds.json')
-    .then(response => response.json())
-    .then(data => {
-      const backgrounds = data.backgrounds;
-      const glitchImgs = document.querySelectorAll(".glitch__img");
-	  const num = Math.floor(backgrounds.length * Math.random());
-      glitchImgs.forEach((glitchImg) => {
-        glitchImg.style.backgroundImage = "url(" + backgrounds[num].src + ")";
-        glitchImg.style.backgroundPosition = backgrounds[num].position;
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching backgrounds:', error);
-    });
+  // Fetch the background JSON and set the background, or handle errors if the fetch fails
+  fetchBackgroundJSON()
+    .then(setBackground)
+    .catch(handleJSONFetchError);
 });
+
+// Function to fetch the background JSON data
+function fetchBackgroundJSON() {
+  return fetch('data/backgrounds.json')
+    .then(response => response.json());
+}
+
+// Function to set the background images
+function setBackground(data) {
+  const glitchImgs = document.querySelectorAll(".glitch__img"); // Select all elements with the 'glitch__img' class
+  const background = data.backgrounds[Math.floor(data.backgrounds.length * Math.random())]; // Randomly select a background
+  glitchImgs.forEach(glitchImg => {
+    glitchImg.style.backgroundImage = `url(${background.src})`; // Set background-image
+    glitchImg.style.backgroundPosition = background.position; // Set background-position
+  });
+}
+
+// Function to handle errors during the JSON fetch
+function handleJSONFetchError(error) {
+  console.error('Error fetching backgrounds:', error);
+}
--- js/greeting.js
+++ js/greeting.js
@@ -1,144 +1,105 @@
+// getBrowserLocales adapted from: https://phrase.com/blog/posts/detecting-a-users-locale/#Client-side_The_navigatorlanguages_Object
+// Typing adapted from: https://usefulangle.com/post/75/typing-effect-animation-javascript-css
+
+// Wait for the DOM to be fully loaded before running the script
 document.addEventListener("DOMContentLoaded", function() {
-  // getBrowserLocales adapted from: https://phrase.com/blog/posts/detecting-a-users-locale/#Client-side_The_navigatorlanguages_Object
-  // Typing adapted from: https://usefulangle.com/post/75/typing-effect-animation-javascript-css
+  // Select DOM elements for the text and cursor
+  const textElement = document.querySelector(".greeting__text");
+  const cursorElement = document.querySelector(".greeting__cursor");
+  textElement.innerHTML = "";
 
-  function getBrowserLocales(options = {}) {
-    const defaultOptions = {
-      languageCodeOnly: false,
-    };
-
-    const opt = {
-      ...defaultOptions,
-      ...options,
-    };
-
-    const browserLocales =
-      navigator.languages === undefined
-        ? [navigator.language]
-        : navigator.languages;
-
-    if (!browserLocales) {
-      return undefined;
-    }
-
-    return browserLocales.map((locale) => {
-      const trimmedLocale = locale.trim();
-
-      return opt.languageCodeOnly
-        ? trimmedLocale.split(/-|_/)[0]
-        : trimmedLocale;
-    });
-  }
-
-  function shuffleArray(a) {
-    let j, t;
-    for (let i = a.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      t = a[i];
-      a[i] = a[j];
-      a[j] = t;
-    }
-  }
-
-  function Type() {
-    var text = greetingContents[currentGreeting].message.substring(
-      0,
-      greetingIndex + 1
-    );
-    textElement.style.fontFamily =
-      "'" + greetingContents[currentGreeting].font + "', sans-serif";
-    textElement.innerHTML = text;
-    greetingIndex++;
-    if (text === greetingContents[currentGreeting].message) {
-      cursorElement.style.display = "none";
-      if (greetingContents.length > 1) {
-        clearInterval(currentInterval);
-        setTimeout(function () {
-          currentInterval = setInterval(Delete, 50);
-        }, 3000);
-      }
-    }
-  }
-
-  function Delete() {
-    var text = greetingContents[currentGreeting].message.substring(
-      0,
-      greetingIndex - 1
-    );
-    textElement.innerHTML = text;
-    greetingIndex--;
-    if (text === "") {
-      clearInterval(currentInterval);
-      if (currentGreeting == greetingContents.length - 1) {
-        currentGreeting = 0;
-      } else {
-        currentGreeting++;
-      }
-      greetingIndex = 0;
-      setTimeout(function () {
-        cursorElement.style.display = "inline-block";
-        currentInterval = setInterval(Type, 100);
-      }, 200);
-    }
-  }
-
+  // Fetch greetings from a JSON file
   async function getGreetings() {
     try {
       const response = await fetch("data/greetings.json");
-      const data = await response.json();
-      return data.greetings.map((greeting) => ({
-        message: greeting.message,
-        code: greeting.code,
-        font: greeting.font,
-      }));
+      return (await response.json()).greetings;
     } catch (error) {
-      console.error(error);
-      return [
-        {
-          message: "Hello World!",
-          code: "en",
-          font: "sans-serif",
-        },
-      ];
+      // Log the error and return a default greeting in case of fetch failure
+      console.error('Error fetching greetings:', error);
+      return [{ message: "Hello World!", code: "en", font: "sans-serif" }];
     }
   }
 
-  async function asyncRunner() {
-    // Greetings and language codes
-    greetingContents = await getGreetings();
-
-    // Before we start shuffle all greetings to show no preferences
-    shuffleArray(greetingContents);
-
-    // Move greetings that match the user's languages to the front of the array
-    uniqueLanguages.reverse().forEach((language) =>
-      greetingContents.forEach(function (greeting, i) {
-        if (greeting.code === language) {
-          greetingContents.splice(i, 1);
-          greetingContents.unshift(greeting);
-        }
-      })
-    );
-
-    // Start the type/delete cycle
-    currentInterval = setInterval(Type, 100);
+  // Get the browser's preferred locales
+  function getBrowserLocales(options = { languageCodeOnly: true }) {
+    return (navigator.languages || [navigator.language])
+      .map(locale => options.languageCodeOnly ? locale.split(/-|_/)[0] : locale.trim());
   }
 
-  var currentGreeting = 0;
-  var currentInterval;
-  var greetingIndex = 0;
+  // Shuffle the elements of an array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
 
-  var textElement = document.querySelector(".greeting__text");
-  var cursorElement = document.querySelector(".greeting__cursor");
-  var browserLanguages = getBrowserLocales({ languageCodeOnly: true });
-  var uniqueLanguages =
-    typeof browserLanguages !== "undefined"
-      ? Array.from(new Set(browserLanguages))
-      : ["en"];
+  // Prioritize greetings based on the user's browser languages
+  function prioritizeGreetings(greetings, languages) {
+    return greetings.sort((a, b) => languages.indexOf(b.code) - languages.indexOf(a.code));
+  }
 
-  var greetingContents = [];
+  // Main function to run the typing animation
+  async function asyncRunner() {
+    let greetings = await getGreetings();
+    shuffleArray(greetings);
+    const userLanguages = Array.from(new Set(getBrowserLocales()));
+    greetings = prioritizeGreetings(greetings, userLanguages);
 
-  textElement.innerHTML = "";
+    // Initialize and start the typing animation
+    new TypingAnimation(textElement, cursorElement, greetings).start();
+  }
 
   asyncRunner();
 });
+
+// TypingAnimation class to handle the typing effect
+class TypingAnimation {
+  constructor(textElement, cursorElement, greetings, typingSpeed = 100, deletingSpeed = 50) {
+    this.textElement = textElement;
+    this.cursorElement = cursorElement;
+    this.greetings = greetings;
+    this.currentGreeting = 0;
+    this.greetingIndex = 0;
+    this.typingSpeed = typingSpeed; // Speed of typing in ms
+    this.deletingSpeed = deletingSpeed; // Speed of deleting in ms
+  }
+
+  // Method to handle the typing effect
+  type() {
+    const greeting = this.greetings[this.currentGreeting];
+    let text = greeting.message.substring(0, this.greetingIndex + 1);
+    this.textElement.style.fontFamily = `'${greeting.font}', sans-serif`;
+    this.textElement.innerHTML = text;
+    this.greetingIndex++;
+
+    if (text === greeting.message) {
+      this.cursorElement.style.display = "none";
+      setTimeout(() => this.delete(), 3000); // Pause before deleting
+    } else {
+      setTimeout(() => requestAnimationFrame(() => this.type()), this.typingSpeed);
+    }
+  }
+
+  // Method to handle the deleting effect
+  delete() {
+    const greeting = this.greetings[this.currentGreeting];
+    let text = greeting.message.substring(0, this.greetingIndex - 1);
+    this.textElement.innerHTML = text;
+    this.greetingIndex--;
+
+    if (text === "") {
+      this.currentGreeting = (this.currentGreeting + 1) % this.greetings.length;
+      this.greetingIndex = 0;
+      this.cursorElement.style.display = "inline-block";
+      setTimeout(() => this.type(), 200); // Pause before typing next greeting
+    } else {
+      setTimeout(() => requestAnimationFrame(() => this.delete()), this.deletingSpeed);
+    }
+  }
+
+  // Start the typing animation
+  start() {
+    requestAnimationFrame(() => this.type());
+  }
+}
